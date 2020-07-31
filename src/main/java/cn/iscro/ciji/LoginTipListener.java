7707460bf19b06ab9266dceb1d7cdb5f98c84120
ImOxygen233 <chenpeichen2007@outlook.com>
2020-07-31 22:57:41 +0800

    新增每隔5s提示一次需要登录

--- src/main/java/cn/iscro/ciji/LoginTipListener.java
+++ src/main/java/cn/iscro/ciji/LoginTipListener.java
@@ -1,11 +1,17 @@
 package cn.iscro.ciji;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.concurrent.TimeUnit;
+
 public class LoginTipListener implements Listener {
     @EventHandler
-    private void OnPlayerJoin(PlayerJoinEvent ThisEvent){
-        ThisEvent.getPlayer().sendMessage(LoginWelcome.isRegister(ThisEvent.getPlayer().getName())?"欢迎来到本服！请输入/l <密码>进行登录":"欢迎来到本服！请输入/reg <密码>进行注册");
+    private void OnPlayerJoin(PlayerJoinEvent ThisEvent) throws InterruptedException {
+        while (!LoginWelcome.isLogin(ThisEvent.getPlayer().getName())) {
+            ThisEvent.getPlayer().sendMessage(LoginWelcome.isRegister(ThisEvent.getPlayer().getName()) ? "欢迎来到本服！请输入/l <密码>进行登录" : "欢迎来到本服！请输入/reg <密码>进行注册");
+            TimeUnit.MILLISECONDS.sleep(5000);
+        }
     }
 }
